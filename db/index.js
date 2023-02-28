003b06594e416f813ebbc206fe40facde22834ab
Muhammad Aswan <muhaswan190897@gmail.com>
2023-02-28 08:02:48 +0800

    install express generator, custom routes, and first config

---
+++ db/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { urlDb } = require('../config');
+
+mongoose.connect(urlDb, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	// useFindAndModify: true,
+	// useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+module.exports = db;
